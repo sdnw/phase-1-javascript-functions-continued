175991f88bec8e4a52370a5424c70585902b2b1b
Steven <stevendawn98@gmail.com>
2022-05-20 20:52:53 -0400

    finished functions cont

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ function saturdayFun(act1 = 'roller-skate', act2 = 'bathe my dog') {
     return `This Saturday, I want to ${act1}!`
 }
 
-function mondayWork(arg1 = ){
+function mondayWork(act1 = 'go to the office', act2 = 'work from home'){
+    return `This Monday, I will ${act1}.`
+}
 
+function wrapAdjective(flair = '*') {
+    return function(adj = 'special') {
+        return `You are ${flair}${adj}${flair}!`
+    }
 }
