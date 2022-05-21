49dc91c9f03717afd21347bf46e2324d3d7c1f56
Steven <stevendawn98@gmail.com>
2022-05-20 20:28:10 -0400

    started functions cont

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function saturdayFun(act1 = 'roller-skate', act2 = 'bathe my dog') {
+    return `This Saturday, I want to ${act1}!`
+}
+
+function mondayWork(arg1 = ){
+
+}
